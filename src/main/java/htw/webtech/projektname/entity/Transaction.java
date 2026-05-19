a9256c417db7fceea193020ee15b0fc4aad5a9d1
Alexander Kalinin <Alexander.Kalinin@Student.HTW-Berlin.de>
2026-05-19 18:13:30 +0200

    add ID as primary Key for database

--- src/main/java/htw/webtech/projektname/entity/Transaction.java
+++ src/main/java/htw/webtech/projektname/entity/Transaction.java
@@ -1,20 +1,29 @@
 package htw.webtech.projektname.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private double amount ;
     private String category;
+    private String owner;
+
+    public Transaction() {}
 
     public Transaction(Long id, String title, double amount, String category) {
-        this.id = id;
         this.title = title;
         this.amount = amount;
         this.category = category;
+        this.owner = owner;
     }
 
     public Long getId() { return id; }
     public String getTitle() { return title; }
     public double getAmount() { return amount; }
     public String getCategory() { return category; }
+    public String getOwner() { return owner; }
 }
